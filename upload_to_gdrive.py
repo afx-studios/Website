663cd50582354ff90b1c4a55b2ea118fbe6d0772
Andrew Gwynn <afx.studios.hq@gmail.com>
2024-11-08 15:42:27 -0800

    Create upload_to_gdrive.py

---
+++ upload_to_gdrive.py
@@ -0,0 +1,31 @@
+import os
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+
+# Authenticate using the service account key
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SERVICE_ACCOUNT_FILE = 'service_account.json'
+
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+drive_service = build('drive', 'v3', credentials=credentials)
+
+# File metadata
+file_metadata = {
+    'name': 'website.zip',
+    # Replace 'YOUR_FOLDER_ID' with your actual folder ID
+    'parents': ['YOUR_FOLDER_ID']
+}
+
+media = MediaFileUpload('website.zip', mimetype='application/zip')
+
+# Upload the file
+file = drive_service.files().create(
+    body=file_metadata,
+    media_body=media,
+    fields='id'
+).execute()
+
+print(f"File ID: {file.get('id')}")
